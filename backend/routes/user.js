cf600de9e79f354237885dac880096f2ad10b1ad
Omkar Gadam <omkargangadhargadam@gmail.com>
2024-08-17 00:43:08 +0530

    created middleware and update route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,15 +6,19 @@ const { User } = require("../db");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
+
+router.get("/", (req, res) => {
+  res.send("good to go");
+});
+
+
+//signup
+
 const signupSchema = zod.object({
   username: zod.string(),
   password: zod.string(),
   firstName: zod.string(),
   lastName: zod.string(),
-});
-
-router.get("/", (req, res) => {
-  res.send("good to go");
 });
 
 router.post("/signup", async (req, res) => {
@@ -57,5 +61,65 @@ router.post("/signup", async (req, res) => {
     token: token,
   });
 });
+
+
+//update user
+
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+
+  await User.updateOne({ _id: req.userId }, req.body);
+
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+
+//find user on basis of substring of userfield
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
+
+
+
+
 
 module.exports = router;
